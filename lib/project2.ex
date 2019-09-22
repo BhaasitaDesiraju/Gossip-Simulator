3012f6750091e0aab5952f9cbcc04c39da8c0016
bhaasitadesiraju <bhaasitadesiraju@gmail.com>
2019-09-22 19:39:01 -0400

    Test commit

--- lib/project2.ex
+++ lib/project2.ex
@@ -1,18 +1,3 @@
 defmodule Project2 do
-  @moduledoc """
-  Documentation for Project2.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Project2.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+#code goes here
 end
